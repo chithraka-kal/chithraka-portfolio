8fffcccb2c9372683cc444fefa24f2714ba96b5c
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-23 19:29:58 +0530

    skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+function Skills() {
+    return (
+        <div>
+            <h2>Skills</h2>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>HTML & CSS</li>
+                <li>Git & GitHub</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Skills;
