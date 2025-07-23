fd4b2d05cd8e5ba236c971df12e12dfa4c85e3d0
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-23 19:29:14 +0530

    certification

---
+++ src/components/Certification.jsx
@@ -0,0 +1,9 @@
+function Certification() {
+    return (
+        <div>
+            <h2>Certifications</h2>
+            <p>Certified Full Stack Developer</p>
+        </div>
+    );
+}        
+export default Certification;
