f111acb6176fb3109363481391d8a0ef8ddaaf0d
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-23 19:29:20 +0530

    contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <p>Email: <a href="mailto:1K6sM@example.com">1K6sM@example.com</a></p>
+        </div>
+    );
+}    
+export default Contact;
