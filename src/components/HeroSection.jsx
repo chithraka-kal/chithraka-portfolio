a26f88329d8a466c6833cd288fb95bd2ccc83a46
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-23 19:29:39 +0530

    heroserction

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+    return (
+        <div className="hero-section">
+            <h1>Chithraka Kalanamith</h1>
+            <p>Full Stack Developer | React | Node.js</p>
+        </div>
+    );
+}
+export default HeroSection;
