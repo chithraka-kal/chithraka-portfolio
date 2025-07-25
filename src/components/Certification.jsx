33580b038bb5e44b2f30c8c342b83d8093b79a6e
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-25 16:12:15 +0530

    certification

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -134,10 +134,13 @@ function Certification() {
                                             onClick={() => handleViewCertificate(cert.certificateUrl, cert.name)}
                                             title={`View ${cert.name} certificate`}
                                         >
-                                            <svg className={styles.linkIcon} viewBox="0 0 16 16" width="16" height="16">
-                                                <path d="M3.75 2h3.5a.75.75 0 0 1 0 1.5h-3.5a.25.25 0 0 0-.25.25v8.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25v-3.5a.75.75 0 0 1 1.5 0v3.5A1.75 1.75 0 0 1 12.25 14h-8.5A1.75 1.75 0 0 1 2 12.25v-8.5C2 2.784 2.784 2 3.75 2Zm6.854-1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1Z"></path>
+                                            <svg className={styles.certificateIcon} viewBox="0 0 24 24" width="18" height="18">
+                                                <path fill="currentColor" d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
                                             </svg>
                                             View Certificate
+                                            <svg className={styles.externalIcon} viewBox="0 0 16 16" width="14" height="14">
+                                                <path fill="currentColor" d="M3.75 2h3.5a.75.75 0 0 1 0 1.5h-3.5a.25.25 0 0 0-.25.25v8.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25v-3.5a.75.75 0 0 1 1.5 0v3.5A1.75 1.75 0 0 1 12.25 14h-8.5A1.75 1.75 0 0 1 2 12.25v-8.5C2 2.784 2.784 2 3.75 2Zm6.854-1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1Z"/>
+                                            </svg>
                                         </button>
                                     </div>
                                 </div>
